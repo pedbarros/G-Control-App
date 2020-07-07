49c1c119732d9ed166da738976e7edddb5ce7b57
Pedro Barros <pedro.barros@herospark.com>
2020-07-06 21:19:33 -0300

    add signOut method and use on Dashboard component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import React, { useContext } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import AuthContext from '../../contexts/auth';
 
 const Dashboard: React.FC = () => {
-  return <View />;
-}
+  const { signOut } = useContext(AuthContext);
+
+  function handleSignOut() {
+    signOut();
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Sign Out" onPress={handleSignOut} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 
 export default Dashboard;
