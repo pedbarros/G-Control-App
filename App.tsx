76f15129a0ee3c540afaa3f49e149f0c0037fcf6
Pedro Barros <pedro.barros@herospark.com>
2020-07-06 21:55:47 -0300

    add authorization token on all requests

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
 import { AuthProvider } from './src/contexts/auth';
 
 const App: React.FC = () => {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#000" />
-
       <NavigationContainer>
         <AuthProvider>
           <Routes />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,21 @@ import React, { useContext } from 'react';
 import AuthRoutes from '../routes/auth.routes';
 import AuthContext from '../contexts/auth';
 import AppRoutes from './app.routes';
+import { View, ActivityIndicator } from "react-native";
 
 const Routes: React.FC = () => {
-  const { signed } = useContext(AuthContext)
-  return signed  ? <AppRoutes/> : <AuthRoutes />;
+  const { signed, loading} = useContext(AuthContext);
+
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#666" />
+      </View>
+    );
+  }
+
+  return signed ? <AppRoutes /> : <AuthRoutes />;
 };
 
 export default Routes;
